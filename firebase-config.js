6deca14e99354ae6b1c443445813e52a7a53ef71
DennisM2431 <dendizzle2431@gmail.com>
2026-08-04 14:51:19 +1000

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAov95t-VaSIckKdFkufyziOVMfjAHTcCM",
-  authDomain: "jared-bucks-fairway-mayhem.firebaseapp.com",
-  projectId: "jared-bucks-fairway-mayhem",
-  storageBucket: "jared-bucks-fairway-mayhem.firebasestorage.app",
-  messagingSenderId: "567408024127",
-  appId: "1:567408024127:web:23346fdc6597e3ca4cf68a"
-};
